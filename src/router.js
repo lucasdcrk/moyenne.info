6410c5ed59efbd1d46f1d10a56685ae39f1a44ca
Lucas Decrock <lordapofr@gmail.com>
2018-12-02 17:10:00 +0100

    Updated router

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ import NotFound from './views/NotFound.vue'
 Vue.use(Router);
 
 const router = new Router({
-    mode: 'history',
+    mode: 'hash',
     routes: [
         {
             path: '/',
