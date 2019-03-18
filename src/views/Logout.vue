37a161211022c6d863dff7bdf61f2ec0810b5c26
Lucas Decrock <lordapofr@gmail.com>
2019-03-18 14:48:51 +0100

    Create Logout.vue

---
+++ src/views/Logout.vue
@@ -0,0 +1,10 @@
+<script>
+    export default {
+        mounted() {
+            localStorage.clear();
+            window.bus.$emit('logged-out');
+            this.refreshingSession = false;
+            this.$router.push('/login');
+        }
+    }
+</script>
